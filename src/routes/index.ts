c0b4705c11a706a9c04a407f959defcae58bc0ad
Jonas Pereira <jonasprodrigues@gmail.com>
2020-04-14 12:41:36 +0000

    chore: return tamplate base

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,17 @@
+
 import { Router } from 'express';
-import appointmentsRouter from './appointments.routes'
 
 const routes = Router();
 
-routes.use('/appointments', appointmentsRouter)
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+});
 
 export default routes;
